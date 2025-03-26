6ded1df6e558bb096698dea4f2443321634c23e4
billy-juicy <libertyeternity00@gmail.com>
2025-03-26 12:57:04 +0300

    Add new changes at application

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('first_name', 'last_name')
+
+admin.site.register(Client)
--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    user = models.ForeignKey(User, db_index=True, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Clients"
+        verbose_name = "Client"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
