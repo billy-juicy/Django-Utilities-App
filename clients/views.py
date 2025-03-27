1f5712c7bb7794ab32b47527e86ebaa7d47ab912
billy-juicy <libertyeternity00@gmail.com>
2025-03-27 09:44:25 +0300

    Add an exception to the file views

--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Client
 
 def home(request):
     clients = Client.objects.all() # Получаем всех клиентов
+
+    if not clients: # Исключение
+        raise Http404('Нет клиентов для отображения!')
+
     return render(request, 'clients/home.html', {'clients': clients}) # Передаем всех клиентов в шаблон
