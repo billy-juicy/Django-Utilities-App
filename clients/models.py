de634c8ae68c89cd757a252d496ba56e4ceb31ca
billy-juicy <libertyeternity00@gmail.com>
2025-03-27 09:32:02 +0300

    Add comments and changes in a few files

--- clients/models.py
+++ clients/models.py
@@ -2,12 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Client(models.Model):
-    last_name = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=100)
-    middle_name = models.CharField(max_length=100, blank=True, null=True)
-    phone = models.CharField(max_length=20)
-    address = models.TextField()
-    user = models.ForeignKey(User, db_index=True, on_delete=models.CASCADE)
+    last_name = models.CharField(max_length=100) # Фамилия
+    first_name = models.CharField(max_length=100) # Имя
+    middle_name = models.CharField(max_length=100, blank=True, null=True) # Отчество с необязательным заполнением
+    phone = models.CharField(max_length=20) # Телефон
+    address = models.TextField() # Адрес
+    user = models.ForeignKey(User, db_index=True, on_delete=models.CASCADE) # Внешний ключ к таблице Пользователи
 
     class Meta:
         verbose_name_plural = "Clients"
--- clients/urls.py
+++ clients/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import home
-from ..practice.urls import urlpatterns
 
 urlpatterns = [
     path('', home, name='home'), # Главная страница
