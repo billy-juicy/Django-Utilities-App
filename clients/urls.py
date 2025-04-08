bd0e906b6c1dce03221444b5dc31f709f238355d
billy.juicy <166807779+billy-juicy@users.noreply.github.com>
2025-04-08 12:09:30 +0300

    Add Jquery and to setting at complaint_add_form.html

--- clients/urls.py
+++ clients/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import ClientListView
 from .views import AddComplaintFormView
+from .views import ClientAutocomplete
 
 urlpatterns = [
     path('list', ClientListView.as_view(), name='clients-list'),
     path('complaint/add', AddComplaintFormView.as_view(), name='complaints-add'),
     path('complaint/success', AddComplaintFormView.as_view(), name='complaints-success'),
+    path(r'autocomplete/', ClientAutocomplete.as_view(), name='clients-autocomplete',)
 ]
